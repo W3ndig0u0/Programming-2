d2f2adb9c67033b568c940461ebb7961eb8219b1
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-08-30 13:15:47 +0200

    asdf

---
+++ Test/Test/Player.cs
@@ -0,0 +1,18 @@
+namespace Test
+{
+  public class Player
+  {
+    public int currHoldingInv;
+    public int currAmountInv;
+    public int maxInv;
+    public int damage;
+    public int maxHp;
+    public int currHp;
+    public string currAmountInvName;
+    public string name;
+    public bool inAir;
+    public bool runningAvailable;
+    public bool jumpingAvailable;
+    public bool powerAvailable;
+  }
+}
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,15 +7,37 @@ namespace Test
     static void Main(string[] args)
     {
       Yo Yo1 = new Yo();
+      Player player = new Player();
 
       string yooo = Yo1.yoo;
-      Console.WriteLine(yooo);
-      Console.WriteLine("Är det sant?");
 
-      Console.WriteLine(Yo1.yooQuestion);
+      player.name = "Bob The builder";
+      player.maxHp = 100;
+      player.currHp = 80;
+      player.damage = 12;
+      player.currHoldingInv = 1;
+      player.currAmountInv = 2;
+      player.currAmountInvName = "Sword";
+      player.maxInv = 3;
+      player.inAir = false;
+      player.runningAvailable = true;
+      player.powerAvailable = true;
+      player.jumpingAvailable = true;
 
+      // Console.WriteLine(yooo);
+      // Console.WriteLine("Är det sant?");
+      // Console.WriteLine(Yo1.yooQuestion);
+
+      Console.WriteLine($"Name: {player.name}");
+      Console.WriteLine($"Health: {player.currHp} / {player.maxHp} hp");
+      Console.WriteLine($"Currently Holding Inventory Nr: {player.currHoldingInv},  {player.currAmountInvName} {player.damage} dmg");
+      Console.WriteLine($"Current Amount of Inventory: {player.currAmountInv}");
+      Console.WriteLine($"Max Inventory: {player.maxInv}");
+      Console.WriteLine($"Currently in the air: {player.inAir}");
+      Console.WriteLine($"Running Avalible: {player.runningAvailable}");
+      Console.WriteLine($"Jumping Avalible: {player.jumpingAvailable}");
+      Console.WriteLine($"Special Powers Avalible: {player.powerAvailable}");
       Console.ReadKey();
-
 
     }
   }
