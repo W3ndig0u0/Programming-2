e64f16f7b28e811d8aaa03fb9a370302bde4d0d1
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-09-02 10:19:20 +0200

    jkl

---
+++ Test/Test/SpaceShip.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Test
+{
+    public class SpaceShip
+    {
+        public int dmg;
+        public int maxHp;
+        public int xPos;
+        public int yPos;
+        public string name;
+        public bool inAir;
+        public bool shootAvailable;
+    }
+}
